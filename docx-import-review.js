e70b9f3a015e55ce1de7c800ac686252bfe1b9b7
sunilbane19 <sunilbane19@gmail.com>
2026-09-16 15:11:21 +0300

    Bust DOCX parser cache for section fix

--- docx-import-review.js
+++ docx-import-review.js
@@ -1,6 +1,6 @@
 import { supabase as sb } from './supabase-client-legacy.js?v=1.0.0';
 import * as mammoth from 'https://esm.sh/mammoth@1.6.0';
-import { parseDocx } from './docx-parser.js?v=1.0.1';
+import { parseDocx } from './docx-parser.js?v=1.0.2';
 
 const dialog=document.querySelector('#detailDialog');
 const esc=s=>String(s??'').replace(/[&<>\"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','\"':'&quot;',"'":'&#39;'}[c]));
